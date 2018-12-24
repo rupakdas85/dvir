10ab1040b44d9e4a10b8bf32e44ec30e06577fee
Rupak <rupakkumar.das@capita.co.uk>
2018-12-24 18:33:13 +0530

    Driver pre inspection page update

--- src/app/driver/driver.module.ts
+++ src/app/driver/driver.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PreTripInspectionComponent } from './pre-trip-inspection/pre-trip-inspection.component';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   imports: [
     CommonModule,
+    InputTextModule
   ],
   declarations: [PreTripInspectionComponent]
 })
--- src/app/driver/pre-trip-inspection/pre-trip-inspection.component.ts
+++ src/app/driver/pre-trip-inspection/pre-trip-inspection.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-pre-trip-inspection',
   templateUrl: './pre-trip-inspection.component.html',
