e1319752a2086f506e4b3c74b62be3cba003b89f
Rupak <rupakkumar.das@capita.co.uk>
2018-12-27 20:31:17 +0530

    defect check form update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
+import { FormsModule } from '@angular/forms';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppRouting } from './app.routes';
 import { DriverModule } from './driver/driver.module';
 import { AppComponent } from './app.component';
+
+
 
 
 @NgModule({
@@ -14,6 +17,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
+    FormsModule,
     AppRouting,
     DriverModule,
     AngularFontAwesomeModule
